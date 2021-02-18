113d10a894a7d61764a00f24f909998acdd98cce
S4KITA <ekisi1120!>
2021-02-18 23:10:39 +0900

    210218 11:10 S4KITA

--- app/src/main/java/com/example/gonggong/MainActivity.java
+++ app/src/main/java/com/example/gonggong/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.gonggong;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            default:
+                break;
+        }
     }
 }
