2030115f16c2a1ad8be2741e42371a9b546f541f
S4KITA <ekisi1120!>
2021-02-18 23:35:59 +0900

    210218 11:35 S4KITA

--- app/src/main/java/com/example/gonggong/MainActivity.java
+++ app/src/main/java/com/example/gonggong/MainActivity.java
@@ -3,21 +3,38 @@ package com.example.gonggong;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button mButtonKakaoLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.mButtonKakaoLogin:
+                Log.i("GongGong","카카오 로그인");
+                break;
+
             default:
                 break;
         }
+    }
+
+    private void init() {
+
+        mButtonKakaoLogin = (Button) findViewById(R.id.mButtonKakaoLogin);
+        mButtonKakaoLogin.setOnClickListener(this);
+
     }
 }
