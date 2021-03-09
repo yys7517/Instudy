fce75ee732153c6b3f6150de33921e3b8d3332b1
S4KITA <ekisi1120!>
2021-03-10 00:52:56 +0900

    0310 이현호 수정

--- app/src/main/java/com/example/gonggong/ui/calender/BottomNavigationFrag3.java
+++ app/src/main/java/com/example/gonggong/ui/calender/BottomNavigationFrag3.java
@@ -1,17 +1,140 @@
 package com.example.gonggong.ui.calender;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
 import com.example.gonggong.R;
+import com.example.gonggong.ui.calender.decorators.EventDecorator;
+import com.example.gonggong.ui.calender.decorators.OneDayDecorator;
+import com.example.gonggong.ui.calender.decorators.SaturdayDecorator;
+import com.example.gonggong.ui.calender.decorators.SundayDecorator;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.Executors;
 
 public class BottomNavigationFrag3 extends Fragment {
+
+    private MaterialCalendarView mMaterialCalendarView;
+    private Button mButtonTest;
+
+    private SundayDecorator mSundayDecorator = new SundayDecorator();
+    private SaturdayDecorator mSaturdayDecorator = new SaturdayDecorator();
+    private OneDayDecorator mOneDayDecorator = new OneDayDecorator();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_bottomnavigationfrag3, container, false);
 
+        mMaterialCalendarView = (MaterialCalendarView) rootView.findViewById(R.id.mMaterialCalendarView);
+
+        mMaterialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(2017, 0, 1)) // 달력의 시작
+                .setMaximumDate(CalendarDay.from(2030, 11, 31)) // 달력의 끝
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        mMaterialCalendarView.addDecorators(
+                mSundayDecorator,
+                mSaturdayDecorator,
+                mOneDayDecorator
+        );
+
+        final String[] result = {"2021,03,18", "2021,04,18", "2021,05,18", "2021,06,18"};
+
+        new ApiSimulator(result).executeOnExecutor(Executors.newSingleThreadExecutor());
+
+        mMaterialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                int Year = date.getYear();
+                int Month = date.getMonth() + 1;
+                int Day = date.getDay();
+
+                Log.i("Year test", Year + "");
+                Log.i("Month test", Month + "");
+                Log.i("Day test", Day + "");
+
+                String shot_Day = Year + "," + Month + "," + Day;
+
+                Log.i("shot_Day test", shot_Day + "");
+                mMaterialCalendarView.clearSelection();
+            }
+        });
+
+        mButtonTest = (Button) rootView.findViewById(R.id.mButtonTest);
+
+        mButtonTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Test", "테스트 메세지");
+            }
+        });
+
         return rootView;
+    }
+
+    private class ApiSimulator extends AsyncTask<Void, Void, List<CalendarDay>> {
+
+        String[] Time_Result;
+
+        ApiSimulator(String[] Time_Result) {
+            this.Time_Result = Time_Result;
+        }
+
+        @Override
+        protected List<CalendarDay> doInBackground(@NonNull Void... voids) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Calendar calendar = Calendar.getInstance();
+            ArrayList<CalendarDay> dates = new ArrayList<>();
+
+            /*특정날짜 달력에 점표시해주는곳*/
+            /*월은 0이 1월 년,일은 그대로*/
+            //string 문자열인 Time_Result 을 받아와서 ,를 기준으로짜르고 string을 int 로 변환
+            for (int i = 0; i < Time_Result.length; i++) {
+                CalendarDay day = CalendarDay.from(calendar);
+                String[] time = Time_Result[i].split(",");
+                int year = Integer.parseInt(time[0]);
+                int month = Integer.parseInt(time[1]);
+                int dayy = Integer.parseInt(time[2]);
+
+                dates.add(day);
+                calendar.set(year, month - 1, dayy);
+            }
+
+            return dates;
+        }
+
+        @Override
+        protected void onPostExecute(@NonNull List<CalendarDay> calendarDays) {
+            super.onPostExecute(calendarDays);
+
+            mMaterialCalendarView.addDecorators(new EventDecorator(Color.GREEN, calendarDays, getActivity()));
+        }
     }
 }
