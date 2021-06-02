0f1a59422aad59bce1860cb20d261ac97a7cf5fe
yonghyeon <qodydgus97@naver.com>
2021-06-02 23:10:20 +0900

    [추가] 프로필 & 프로필수정

--- app/src/main/java/com/example/gonggong/ui/profile/ProfileEdit.java
+++ app/src/main/java/com/example/gonggong/ui/profile/ProfileEdit.java
@@ -2,11 +2,14 @@ package com.example.gonggong.ui.profile;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -16,25 +19,39 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.gonggong.R;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class ProfileEdit extends AppCompatActivity {
 
+    private String USER_ID, USER_NICKNAME, USER_CONTENTS, USER_PROFILEURL = null, USER_TESTID = "Tester01";
+
+    private static String IP_ADDRESS = "211.211.158.42/instudy";
+    private static String TAG1 = "프로필수정 게시물";
+
     ImageView backspace;
     ImageButton imgbtncamera;
-    EditText nickname, profilecontents;
+    TextView mTextViewPostResult;
+    EditText mEditNickname, mEditProfileContents;
     Button validation;
 
     private Boolean isPermission = true;
@@ -43,17 +60,28 @@ public class ProfileEdit extends AppCompatActivity {
     private static final int PICK_FROM_ALBUM = 1;
     private static final int PICK_FROM_CAMERA = 2;
 
+    //SharedPreferences
+    private SharedPreferences appData;
+
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        mTextViewPostResult = findViewById(R.id.mTextViewPostResult); //실패 성공 알려줌 하지만 안씀
+
+        Intent mIntent = getIntent(); //인텐트 frag4에서 해줘야함
+        String Default_nickname = mIntent.getStringExtra("nickname");
+        String Defalut_contents = mIntent.getStringExtra("contents");
+        Log.i("TAG1", "NICK : " + Default_nickname);
+        Log.i("TAG1", "CONTENTS : " + Defalut_contents);
 
         //프로필 이미지 선택&변경
         imgbtncamera = (ImageButton) findViewById(R.id.imgbtnCamera);
         imgbtncamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(isPermission) goToAlbum();
+                if (isPermission) goToAlbum();
                 else Toast.makeText(view.getContext(), getResources()
                         .getString(R.string.permission_2), Toast.LENGTH_LONG).show();
             }
@@ -69,20 +97,41 @@ public class ProfileEdit extends AppCompatActivity {
         });
 
         //EditText
-        nickname = (EditText)findViewById(R.id.nicknameViewedit);
-        profilecontents = (EditText)findViewById(R.id.edtSetProfileContents);
+        mEditNickname = (EditText) findViewById(R.id.nicknameViewedit);
+        mEditProfileContents = (EditText) findViewById(R.id.edtSetProfileContents);
+        mEditNickname.setText(Default_nickname);                //intent 닉네임값 받아옴
+        mEditProfileContents.setText(Defalut_contents);         //intent contents값 받아옴
+
+        //SharedPreferences
+        appData = getSharedPreferences("appData", MODE_PRIVATE);
+        String userid = appData.getString("ID", ""); // App 사용자 ID
 
         //확인버튼
-        validation = (Button)findViewById(R.id.mButtonStart);
+        validation = (Button) findViewById(R.id.mButtonStart);
         validation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                USER_NICKNAME = mEditNickname.getText().toString();         //바뀐 닉네임 값 가져오기
+                USER_CONTENTS = mEditProfileContents.getText().toString();  //바뀐 소개 값 가져오기
+//              USER_PROFILEURL // 미구현
+                USER_ID = userid;                       // App 사용자 ID값 가져와서 인자 값으로 넣어주기
 
+
+                if (USER_NICKNAME.length() < 2) {
+                    Toast.makeText(getApplicationContext(), "닉네임을 더 길게 입력해주세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    EditData task = new EditData();
+                    task.execute("http://" + IP_ADDRESS + "/UserModifyAndroid.php", USER_TESTID, USER_NICKNAME, USER_CONTENTS, USER_PROFILEURL);
+                    Toast.makeText(getApplicationContext(), "프로필이 수정되었습니다.", Toast.LENGTH_SHORT).show();
+                    save(USER_NICKNAME);
+                    finish();
+                }
             }
         });
     }
 
-    public void editfin(){
+
+    public void editfin() {
         finish();
         //아래로 내려가면서 사라진다.
         overridePendingTransition(R.anim.none, R.anim.vertical_out);
@@ -91,7 +140,7 @@ public class ProfileEdit extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        if(isFinishing()){
+        if (isFinishing()) {
             //back 버튼을 누르면 화면 종료가 야기되면 동작
             overridePendingTransition(R.anim.none, R.anim.vertical_out);
         }
@@ -158,7 +207,7 @@ public class ProfileEdit extends AppCompatActivity {
     }
 
     /**
-     *  앨범에서 이미지 가져오기
+     * 앨범에서 이미지 가져오기
      */
     private void goToAlbum() {
 
@@ -169,7 +218,7 @@ public class ProfileEdit extends AppCompatActivity {
 
 
     /**
-     *  카메라에서 이미지 가져오기 @@@@현재 오류있음!!@@@@
+     * 카메라에서 이미지 가져오기 @@@@현재 오류있음!!@@@@
      */
     private void takePhoto() {
 
@@ -191,7 +240,7 @@ public class ProfileEdit extends AppCompatActivity {
     }
 
     /**
-     *  폴더 및 파일 만들기
+     * 폴더 및 파일 만들기
      */
     private File createImageFile() throws IOException {
 
@@ -211,7 +260,7 @@ public class ProfileEdit extends AppCompatActivity {
     }
 
     /**
-     *  tempFile 을 bitmap 으로 변환 후 ImageView 에 설정한다.
+     * tempFile 을 bitmap 으로 변환 후 ImageView 에 설정한다.
      */
     private void setImage() {
 
@@ -233,7 +282,7 @@ public class ProfileEdit extends AppCompatActivity {
     }
 
     /**
-     *  권한 설정
+     * 권한 설정
      */
     private void tedPermission() {
 
@@ -262,20 +311,99 @@ public class ProfileEdit extends AppCompatActivity {
 
     }
 
-    private void editTexts() {
-        String editprofile;
-        String editcontents;
+    //사용자 닉네임 값 App에 Save
+    private void save(String nickname) {
+        // SharedPreferences 객체만으론 저장 불가능 Editor 사용
+        SharedPreferences.Editor editor = appData.edit();
 
-        editprofile = nickname.getText().toString(); //닉네임
-        editcontents = profilecontents.getText().toString(); //소개
+        editor.putString("NICKNAME", nickname);
 
-        if(editprofile.length()<2){
-            //0자 or 1자일때
-            Toast myToast = Toast.makeText(this.getApplicationContext(), "2글자 이상 입력해주세요.", Toast.LENGTH_SHORT);
-            myToast.show();
-        } else{
-            //2글자 이상일때
-            //DB에 값을 넣어줌
+        // apply, commit 을 안하면 변경된 내용이 저장되지 않음
+        editor.apply();
+    }
+
+    //닉네임 변경 클래스
+    class EditData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(ProfileEdit.this, "Please Wait", null, true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            mTextViewPostResult.setText(result);
+            Log.d(TAG, "POST response  - " + result);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String serverURL = (String) params[0];
+            String USER_ID = (String) params[1];
+            String USER_NICKNAME = (String) params[2];
+            String USER_CONTENTS = (String) params[3];
+            String USER_PROFILE = (String) params[4];
+
+
+            String postParameters = "UserID=" + USER_ID + "&UserNickName=" + USER_NICKNAME
+                    + "&UserIntroduce=" + USER_CONTENTS + "&UserProfile=" + USER_PROFILE;
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                } else {
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+                bufferedReader.close();
+
+                return sb.toString();
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
         }
     }
 }
