6c93db42ced7324957dce375949532515874e4c7
S4KITA <ekisi1120!>
2021-06-04 18:09:06 +0900

    Calander 성공

---
+++ app/src/main/java/com/example/gonggong/ui/calender/CalanderData.java
@@ -0,0 +1,22 @@
+package com.example.gonggong.ui.calender;
+
+public class CalanderData {
+    private String PostWID;
+    private String PostDate;
+
+    public void setPostWID(String PostWID) {
+        this.PostWID = PostWID;
+    }
+
+    public void setPostDate(String PostDate) {
+        this.PostDate = PostDate;
+    }
+
+    public String getPostWID() {
+        return PostWID;
+    }
+
+    public String getPostDate() {
+        return PostDate;
+    }
+}
