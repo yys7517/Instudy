f80d172983cc68dd098620b720437baea7d70384
S4KITA <ekisi1120!>
2021-06-04 23:10:30 +0900

    Calander 성공

--- app/src/main/java/com/example/gonggong/ui/calender/decorators/EventDecorator.java
+++ app/src/main/java/com/example/gonggong/ui/calender/decorators/EventDecorator.java
@@ -1,27 +1,20 @@
 package com.example.gonggong.ui.calender.decorators;
 
-import android.app.Activity;
-import android.graphics.drawable.Drawable;
 
-import com.example.gonggong.R;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 import java.util.Collection;
 import java.util.HashSet;
 
-/**
- * Decorate several days with a dot
- */
 public class EventDecorator implements DayViewDecorator {
 
-    private final Drawable drawable;
-    private int color;
-    private HashSet<CalendarDay> dates;
+    private final int color;
+    private final HashSet<CalendarDay> dates;
 
-    public EventDecorator(int color, Collection<CalendarDay> dates,Activity context) {
-        drawable = context.getResources().getDrawable(R.drawable.more);
+    public EventDecorator(int color, Collection<CalendarDay> dates) {
         this.color = color;
         this.dates = new HashSet<>(dates);
     }
@@ -33,7 +26,6 @@ public class EventDecorator implements DayViewDecorator {
 
     @Override
     public void decorate(DayViewFacade view) {
-        view.setSelectionDrawable(drawable);
-        //view.addSpan(new DotSpan(5, color)); // 날자밑에 점
+        view.addSpan(new DotSpan(5, color));
     }
 }
